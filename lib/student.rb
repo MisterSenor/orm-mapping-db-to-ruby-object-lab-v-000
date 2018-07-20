5a561144aced77b3a0ca52f324e940db740965a6
Nathan  Visak <nathan.visak@gmail.com>
2018-07-20 15:04:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ class Student
 
   def self.students_below_12th_grade
     sql = "SELECT * FROM students WHERE grade < 12"
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
   end
