f7b20015f16f385a4209e3b24d282ec8c5eea59a
Nathan  Visak <nathan.visak@gmail.com>
2018-07-20 15:22:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -89,5 +89,15 @@ class Student
       self.new_from_db(first_student)
     end
 
+    def self.all_students_in_grade_X(grade)
+      sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE grade = ?
+        SQL
+
+      DB[:conn].execute(sql, grade)
+
+
 
 end
